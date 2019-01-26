6cc0608e16d1dd9d0494a5646e7bb308d7ea48b1
chenhao1024 <brunetsheet@qq.com>
2019-01-26 14:40:04 +0800

    form two way data binding

--- src/app/form/quick-start/quick-start.component.ts
+++ src/app/form/quick-start/quick-start.component.ts
@@ -1,18 +1,18 @@
 import { Component, OnInit } from '@angular/core';
-
+import {RegisterModel} from './model/register-model';
 @Component({
   selector: 'app-quick-start',
   templateUrl: './quick-start.component.html',
   styleUrls: ['./quick-start.component.scss']
 })
 export class QuickStartComponent implements OnInit {
-  userName: string;
+  regModel: RegisterModel = new RegisterModel();
   constructor() { }
 
   ngOnInit() {
   }
-  userNameChange(event): void {
-    console.log(event);
-    this.userName = event.target.value;
-  }
+  // userNameChange(event): void {
+  //   console.log(event);
+  //   this.userName = event.target.value;
+  // }
 }
